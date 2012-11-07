1478f4d3c12afc71259d48db0522049c00222a8b
Scott Pustay <scott.pustay@gmail.com>
2012-11-06 20:49:21 -0800

    proper commenting

--- serial_reader.py
+++ serial_reader.py
@@ -2,7 +2,7 @@ import serial
 import time
 import datetime
 
-ser = serial.Serial('/dev/ttyACM0',9600,timeout=1) // On Ubuntu systems, /dev/ttyACM0 is the default path to the serial device on Arduinos, yours is likely different.
+ser = serial.Serial('/dev/ttyACM0',9600,timeout=1) # On Ubuntu systems, /dev/ttyACM0 is the default path to the serial device on Arduinos, yours is likely different.
 while 1:
     time.sleep(10)
 
